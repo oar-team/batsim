8130cfdea68d696e45b4e7e215a789494897c50c
Michael Mercier <michael.mercier@inria.fr>
2015-06-09 09:54:21 +0200

    remove typo on comments

--- batsim.c
+++ batsim.c
@@ -1,7 +1,7 @@
 /* Copyright (c) 2007-2014. The SimGrid Team and OAR Team
  * All rights reserved.                                                     */
 
-/* This program is free software; you can redistribute it and/or motask_data_tdify it
+/* This program is free software; you can redistribute it and/or modify it
  * under the terms of the license (GNU LGPL) which comes with this package. */
 
 #include "batsim.h"
