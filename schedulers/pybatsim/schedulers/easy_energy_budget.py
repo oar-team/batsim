646412acdaf71550c9257dcb4e1c43263d33e1dd
David Glesser <david.glesser@imag.fr>
2016-02-10 10:29:58 +0100

    Improve powercap algorithm of energy budget shceduler

--- schedulers/pybatsim/schedulers/easy_energy_budget.py
+++ schedulers/pybatsim/schedulers/easy_energy_budget.py
@@ -7,15 +7,17 @@ class EasyEnergyBudget(EasyBackfill):
     def __init__(self, options):
         self.options = options
         
-        self.budget_total = 200*2*100+100*5*100
+        self.budget_total = 200*1*20+100*6*20
         self.budget_left = self.budget_total
-        self.budget_start = 0
-        self.budget_end = 100
+        self.budget_start = 10
+        self.budget_end = 30
         
         self.power_idle = 100
         self.power_compute = 200
         
         self.powercap = self.budget_total/(self.budget_end-self.budget_start)
+        
+        self.already_asked_to_awaken_at_end_of_budget = False
         
         super(EasyEnergyBudget, self).__init__(options)
 
@@ -25,13 +27,14 @@ class EasyEnergyBudget(EasyBackfill):
         self._schedule_jobs(current_time)
 
 
-    def estimate_if_job_fit_in_energy(self, job, current_time, listFreeSpace=None):
+    def estimate_if_job_fit_in_energy(self, job, start_time, listFreeSpace=None):
         """
         compute the power consumption of the cluster
         if <job> is not Non,e then add the power consumption of this job as if it were running on the cluster.
         """
         
-        if not(current_time >= self.budget_start and current_time < self.budget_end):
+        #if the job does not cross the budget interval
+        if not(start_time < self.budget_end and self.budget_start <= start_time+job.requested_time):
             return True
         
         if listFreeSpace is None:
@@ -51,11 +54,7 @@ class EasyEnergyBudget(EasyBackfill):
         if not self.estimate_if_job_fit_in_energy(job, current_time):
             return None
         
-        for l in self.listFreeSpace.generator():
-            if job.requested_resources <= l.res:
-                alloc = self.listFreeSpace.assignJob(l, job, current_time)
-                return alloc
-        return None
+        return super(EasyEnergyBudget, self).allocJobFCFS(job, current_time)
 
     def allocJobBackfill(self, job, current_time):
         #overrinding parent method
@@ -63,26 +62,18 @@ class EasyEnergyBudget(EasyBackfill):
         if not self.estimate_if_job_fit_in_energy(job, current_time):
             return None
         
-        
-        for l in self.listFreeSpace.generator():
-            if job.requested_resources <= l.res and job.requested_time <= l.length:
-                alloc = self.listFreeSpace.assignJob(l, job, current_time)
-                return alloc
-        return None
+        return super(EasyEnergyBudget, self).allocJobBackfill(job, current_time)
 
     def findAllocFuture(self, job):
         #overrinding parent method
 
         listFreeSpaceTemp = copy.deepcopy(self.listFreeSpace)
         
-        print "GOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO"
-        print listFreeSpaceTemp.printme()
-        
         if len(self.listRunningJob) == 0:
-            print "ZEROOOOOO"
             #this condition happen if the powercap is so low that no job can be started now (perhaps some jobs will be backfilled, but we do not it yet)
             #we will ask batsim to wake up us at the end of the budget interval, just to be sure.
-            if self.bs.time() < self.budget_end:
+            if (self.bs.time() < self.budget_end) and not self.already_asked_to_awaken_at_end_of_budget:
+                self.already_asked_to_awaken_at_end_of_budget = True
                 self.bs.wake_me_up_at(self.budget_end)
             alloc = listFreeSpaceTemp.assignJob(listFreeSpaceTemp.firstItem, job, self.budget_end)
             #we find a valid allocation
@@ -90,14 +81,11 @@ class EasyEnergyBudget(EasyBackfill):
 
         previous_current_time = self.bs.time()
         for j in self.listRunningJob:
-            print "now removing: "+str(j)
             new_free_space_created_by_this_unallocation = listFreeSpaceTemp.unassignJob(j)
 
             fit_in_procs = job.requested_resources <= new_free_space_created_by_this_unallocation.res
             
             fit_in_energy = self.estimate_if_job_fit_in_energy(job, j.finish_time, listFreeSpaceTemp)
-
-            print "P", fit_in_procs, "E", fit_in_energy, "Be", self.budget_end, "<", j.finish_time
 
             if fit_in_procs and fit_in_energy:
                 alloc = listFreeSpaceTemp.assignJob(new_free_space_created_by_this_unallocation, job, j.finish_time)
