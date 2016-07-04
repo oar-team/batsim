b03e17813914646fc58e23895288110387316325
Millian Poquet <millian.poquet@inria.fr>
2016-07-04 11:16:32 +0200

    Tests: socket in output_dir, script to run them

---
+++ run_tests.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash -e
+
+# Clean test output directories
+\rm -rf test/out/unique
+\rm -rf test/out/no_energy
+\rm -rf test/out/space_sharing
+\rm -rf test/out/energy
+
+# Run different tests
+tools/experiments/execute_instances.py test/test_unique.yaml -bod test/out/unique
+tools/experiments/execute_instances.py test/test_no_energy.yaml -bod test/out/no_energy
+tools/experiments/execute_instances.py test/test_space_sharing.yaml -bod test/out/space_sharing
+tools/experiments/execute_instances.py test/test_energy.yaml -bod test/out/energy
