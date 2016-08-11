a23ab3b6c976bb090cb5df04ab396a51d050fe5f
henricasanova <henric@hawaii.edu>
2016-08-10 14:59:00 -1000

    Preliminary definition of the Workflow and Task classes

--- src/workflow.hpp
+++ src/workflow.hpp
@@ -8,14 +8,11 @@
 #include <string>
 #include <map>
 
-class Jobs;
-struct Job;
-class Profiles;
-struct JobIdentifier;
-struct BatsimContext;
+class Job;
 
 /**
- * @brief A workflow is simply some Jobs with their associated Profiles
+ * @brief A workflow is a DAG of tasks, with points to
+ *        source tasks and sink tasks
  */
 class Workflow
 {
@@ -31,166 +28,60 @@ public:
     ~Workflow();
 
     /**
-     * @brief Loads a complete workflow from a XML filename
+     * @brief Loads a complete workflow from an XML filename
      * @param[in] xml_filename The name of the XML file
-     * @param[out] nb_machines The number of machines described in the XML file
      */
-    void load_from_xml(const std::string & xml_filename,
-                        int & nb_machines);
+    void load_from_xml(const std::string & xml_filename)
 
     /**
-     * @brief Registers SMPI applications
-     */
-  //    void register_smpi_applications();
-
-    /**
-     * @brief Checks whether a Workflow is valid
+     * @brief Checks whether a Workflow is valid (not needed since loading from XML?)
      */
     void check_validity();
 
+
 public:
     std::string name; //!< The Workflow name
-    Jobs * jobs = nullptr; //!< The Jobs of the Workflow
-    Profiles * profiles = nullptr; //!< The Profiles associated to the Jobs of the Workflow
+    Task * tasks = nullptr; //!< ALL tasks of the Workflow
+    Task * sources = nullptr; //!< The source tasks of the workflow
+    Task * sinks = nullptr; //!< The sink tasks of the workflow
+
 };
 
-
 /**
- * @brief Handles a set of Workflows, identified by their names
+ * @brief A workflow Task is some attributes, pointers to parent tasks,
+ *        and pointers to children tasks.
  */
-class Workflows
+class Task
 {
 public:
     /**
-     * @brief Builds an empty Workflows
+     * @brief Constructor
      */
-    Workflows();
+    Task(int num_procs, double execution_time, Job batsim_job);
 
     /**
-     * @brief Destroys a Workflows
+     * @brief Destructeur
      */
-    ~Workflows();
+    ~Task();
 
     /**
-     * @brief Allows to access a Workflow thanks to its name
-     * @param[in] workflow_name The name of the workflow to access
-     * @return The workflow associated with the given workflow name
-     * @pre The workflow exists
+     * @brief Add a parent to a task
      */
-    Workflow * operator[](const std::string & workflow_name);
+    void add_parent(Task parent_task);
 
     /**
-     * @brief Allows to access a Workflow thanks to its name
-     * @param[in] workflow_name The name of the workflow to access
-     * @return The workflow associated with the given workflow name
-     * @pre The workflow exists
+     * @brief Add a child to a task
      */
-    const Workflow * operator[](const std::string & workflow_name) const;
+    void add_child(Task child_task);
 
-    /**
-     * @brief Allows to access a Workflow thanks to its name
-     * @param[in] workflow_name The name of the workflow to access
-     * @return The workflow associated with the given workflow name
-     * @pre The workflow exists
-     */
-    Workflow * at(const std::string & workflow_name);
 
-    /**
-     * @brief Allows to access a Workflow thanks to its name
-     * @param[in] workflow_name The name of the workflow to access
-     * @return The workflow associated with the given workflow name
-     * @pre The workflow exists
-     */
-    const Workflow * at(const std::string & workflow_name) const;
+public:
+    int num_procs; //!< The number of processors needed for the tas
+    double execution_time; //!< The execution time of the task
+    Job batsim_job; //!< The batsim job created for this task
+    Task * parents = nullptr; //!< The parents
+    Task * children = nullptr; //!< The children
 
-    /**
-     * @brief Allows to get a job from the Workflows
-     * @param[in] workflow_name The name of the workflow the job is in
-     * @param[in] job_number The job number within its workflow
-     * @return The job which has been asked
-     * @pre The requested job exists
-     */
-    Job * job_at(const std::string & workflow_name, int job_number);
-
-    /**
-     * @brief Allows to get a job from the Workflows (const version)
-     * @param[in] workflow_name The name of the workflow the job is in
-     * @param[in] job_number The job number within its workflow
-     * @return The (const) job which has been asked
-     * @pre The requested job exists
-     */
-    const Job * job_at(const std::string & workflow_name, int job_number) const;
-
-    /**
-     * @brief Allows to get a job from the Workflows
-     * @param[in] job_id The JobIdentifier
-     * @return The job which has been asked
-     * @pre The requested job exists
-     */
-    Job * job_at(const JobIdentifier & job_id);
-
-    /**
-     * @brief Allows to get a job from the Workflows (const version)
-     * @param[in] job_id The JobIdentifier
-     * @return The (const) job which has been asked
-     * @pre The requested job exists
-     */
-    const Job * job_at(const JobIdentifier & job_id) const;
-
-    /**
-     * @brief Checks whether a job exist
-     * @param[in] workflow_name The name of the workflow in which the job should be
-     * @param[in] job_number The job number
-     * @return True if the given job exists, false otherwise
-     */
-    bool job_exists(const std::string & workflow_name,
-                    const int job_number);
-
-    /**
-     * @brief Checks whether a job exist
-     * @param[in] job_id The JobIdentifier
-     * @return True if the given job exists, false otherwise
-     */
-    bool job_exists(const JobIdentifier & job_id);
-
-    /**
-     * @brief Adds a job into memory if needed
-     * @details If the jobs already exists, this method does nothing. Otherwise, the job information is loaded from the remote data storage, one Job and one Profile are created and one Workflow is created if needed.
-     * @param[in] job_id The job identifier
-     * @param[in,out] context The Batsim Context
-     * @return The Job corresponding to job_id.
-     */
-    Job * add_job_if_not_exists(const JobIdentifier & job_id,
-                                BatsimContext * context);
-
-    /**
-     * @brief Inserts a new Workflow into a Workflows
-     * @param[in] workflow_name The name of the new Workflow to insert
-     * @param[in] workflow The Workflow to insert
-     * @pre There should be no existing Workflow with the same name in the Workflows
-     */
-    void insert_workflow(const std::string & workflow_name,
-                         Workflow * workflow);
-
-    /**
-     * @brief Checks whether a Workflow with the given name exist.
-     * @param[in] workflow_name The name of the Workflow whose existence is checked
-     * @return true if a Workflow with the given name exists in the Workflows, false otherwise.
-     */
-    bool exists(const std::string & workflow_name) const;
-
-    /**
-     * @brief Gets the internal map
-     * @return The internal map
-     */
-    std::map<std::string, Workflow*> & workflows();
-
-    /**
-     * @brief Gets the internal map (const version)
-     * @return The internal map (const version)
-     */
-    const std::map<std::string, Workflow*> & workflows() const;
-
-private:
-    std::map<std::string, Workflow*> _workflows; //!< Associates Workflows with their names
 };
+
+
