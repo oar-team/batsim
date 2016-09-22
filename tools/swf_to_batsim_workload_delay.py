b6650a8a7a60ff0a5ca645718073e387bef5611c
Millian Poquet <millian.poquet@inria.fr>
2016-09-22 23:59:52 +0200

    SWF -> Batsim wload delay improvement
    Submission times can now be automatically translated
    via a command-line option

--- tools/swf_to_batsim_workload_delay.py
+++ tools/swf_to_batsim_workload_delay.py
@@ -37,13 +37,25 @@ class SwfField(Enum):
 	THINK_TIME_FROM_PRECEDING_JOB=18
 
 parser = argparse.ArgumentParser(description='Reads a SWF (Standard Workload Format) file and transform it into a JSON Batsim workload (with delay jobs)')
-parser.add_argument('inputSWF', type=argparse.FileType('r'), help='The input SWF file')
+parser.add_argument('inputSWF', type=argparse.FileType('r'),
+                    help='The input SWF file')
 parser.add_argument('outputJSON', type=str, help='The output JSON file')
-parser.add_argument('-jwf', '--jobWalltimeFactor', type=float, default=2, help='Jobs walltimes are computed by the formula max(givenWalltime, jobWalltimeFactor*givenRuntime)')
-parser.add_argument('-gwo', '--givenWalltimeOnly', action="store_true", help='If set, only the given walltime in the trace will be')
-parser.add_argument('-jg', '--jobGrain', type=int, default=1, help='Selects the level of detail we want for jobs. This parameter is used to group jobs that have close running time')
-parser.add_argument('-pf', '--platformSize', type=int, default=None, help='If set, the number of machines to put in the output JSON files is set by this parameter instead of taking the maximum job size')
-parser.add_argument('-i', '--indent', type=int, default=None, help='If set to a non-negative integer, then JSON array elements and object members will be pretty-printed with that indent level. An indent level of 0, or negative, will only insert newlines. The default value (None) selects the most compact representation.')
+parser.add_argument('-jwf', '--jobWalltimeFactor',
+                    type=float, default=2,
+                    help='Jobs walltimes are computed by the formula max(givenWalltime, jobWalltimeFactor*givenRuntime)')
+parser.add_argument('-gwo', '--givenWalltimeOnly',
+                    action="store_true",
+                    help='If set, only the given walltime in the trace will be used')
+parser.add_argument('-jg', '--jobGrain',
+                    type=int, default=1,
+                    help='Selects the level of detail we want for jobs. This parameter is used to group jobs that have close running time')
+parser.add_argument('-pf', '--platformSize', type=int, default=None,
+                    help='If set, the number of machines to put in the output JSON files is set by this parameter instead of taking the maximum job size')
+parser.add_argument('-i', '--indent', type=int, default=None,
+                    help='If set to a non-negative integer, then JSON array elements and object members will be pretty-printed with that indent level. An indent level of 0, or negative, will only insert newlines. The default value (None) selects the most compact representation.')
+parser.add_argument('-t', '--translate_submit_times',
+                    action="store_true",
+                    help="If set, the jobs' submit times will be translated towards 0")
 
 group = parser.add_mutually_exclusive_group()
 group.add_argument("-v", "--verbose", action="store_true")
@@ -64,6 +76,7 @@ jobs = []
 profiles = SortedSet()
 
 nb_jobs_discarded = 0
+minimum_observed_submit_time = float('inf')
 
 # Let's loop over the lines of the input file
 for line in args.inputSWF:
@@ -74,7 +87,8 @@ for line in args.inputSWF:
 		nb_res = int(float(res.group(SwfField.ALLOCATED_PROCESSOR_COUNT.value)))
 		run_time = float(res.group(SwfField.RUN_TIME.value))
 		submit_time = max(0,float(res.group(SwfField.SUBMIT_TIME.value)))
-		walltime = max(args.jobWalltimeFactor*run_time, float(res.group(SwfField.REQUESTED_TIME.value)))
+		walltime = max(args.jobWalltimeFactor*run_time,
+		               float(res.group(SwfField.REQUESTED_TIME.value)))
 
 		if args.givenWalltimeOnly:
 			walltime = float(res.group(SwfField.REQUESTED_TIME.value))
@@ -85,6 +99,8 @@ for line in args.inputSWF:
 
 			job = (currentID, nb_res, run_time, submit_time, profile, walltime)
 			currentID = currentID + 1
+			minimum_observed_submit_time = min(minimum_observed_submit_time,
+			                                   submit_time)
 			jobs.append(job)
 		else:
 			nb_jobs_discarded += 1
@@ -95,14 +111,18 @@ for line in args.inputSWF:
 # Let's generate a list of dictionaries for the jobs
 djobs = list()
 for (job_id, nb_res, run_time, submit_time, profile, walltime) in jobs:
-	djobs.append({'id':job_id, 'subtime':submit_time, 'walltime':walltime, 'res':nb_res, 'profile': str(profile)})
+	djobs.append({'id':job_id,
+	             'subtime':submit_time - minimum_observed_submit_time,
+	             'walltime':walltime, 'res':nb_res,
+	             'profile': str(profile)})
 
 # Let's generate a dict of dictionaries for the profiles
 dprofs = {}
 for profile in profiles:
 	dprofs[str(profile)] = {'type':'delay', 'delay':profile}
 
-platform_size = max([nb_res for (job_id, nb_res, run_time, submit_time, profile, walltime) in jobs])
+platform_size = max([nb_res for (job_id, nb_res, run_time, submit_time,
+                                 profile, walltime) in jobs])
 if args.platformSize != None:
 	if args.platformSize < 1:
 		print('Invalid input: platform size must be strictly positive')
@@ -123,7 +143,8 @@ try:
 	json.dump(data, outFile, indent=args.indent)
 
 	if not args.quiet:
-		print('{} jobs and {} profiles had been created'.format(len(jobs), len(profiles)))
+		print('{} jobs and {} profiles had been created'.format(len(jobs),
+		                                                        len(profiles)))
 		print('{} jobs have been discarded'.format(nb_jobs_discarded))
 
 except IOError:
