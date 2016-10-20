65cb2e7208a075f3b02e290bb333e4327072640a
Pierre-François <pfdutot@imag.fr>
2016-10-21 00:47:24 +0200

    counting function for depth

--- src/job_submitter.cpp
+++ src/job_submitter.cpp
@@ -21,6 +21,13 @@ Task* inc_child (Task *i)
   return i;
 }
 
+Task* bottom_level_f (Task *child, Task *parent)
+{
+  child->depth = std::max( child->depth, (parent->depth)+1 );
+
+  return child;
+}
+
 using namespace std;
 
 int static_job_submitter_process(int argc, char *argv[])
@@ -206,6 +213,7 @@ int workflow_submitter_process(int argc, char *argv[])
 
 	    /* tell them they are closer to being elected */
 	    std::transform (my_kids.begin(),my_kids.end(),my_kids.begin(),inc_child);
+	    std::transform (my_kids.begin(),my_kids.end(),my_kids.begin(), boost::bind(&bottom_level_f, _1, completed_task)));
 
 	    /* look for ready kids */
 	    for (std::vector<Task *>::iterator kiddo=my_kids.begin(); kiddo!=my_kids.end(); ++kiddo)
