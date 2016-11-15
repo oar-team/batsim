42377ccf4110bedda472f6bd8d1f3ec518dbf128
Millian Poquet <millian.poquet@inria.fr>
2016-11-15 17:35:24 +0100

    The machine state visualizer should now work fine

--- tools/visualise_machine_state_changes.R
+++ tools/visualise_machine_state_changes.R
@@ -14,12 +14,23 @@ changes = read.csv(args[1])
 changes_without_duplicated_times = changes[!duplicated(changes$time),]
 melted = melt(changes_without_duplicated_times, id="time")
 
+# http://stackoverflow.com/questions/21887088/generate-a-filled-geom-step
+binded <- rbind(
+  melted,
+  transform(melted[order(melted$time),],
+    time=time - 1e-9,  # required to avoid crazy steps
+    value=ave(value, variable, FUN=function(z) c(z[[1]], head(z, -1L)))
+) )
+
 palette = c("#000000",
             "#c552bf",
             "#b68f00",
             "#ff987b",
             "#97e78b")
 
-pdf(args[2])
-ggplot(melted) + geom_area(aes(x=time,y=value,fill=variable)) + scale_fill_manual(values = palette)
+w=16
+h=10
+
+pdf(args[2], width=w, height=h)
+ggplot(binded) + geom_area(aes(x=time,y=value,fill=variable)) + scale_fill_manual(values = palette)
 dev.off()
