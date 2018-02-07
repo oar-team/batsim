c8e3c2ada4e2cc861e80b0596ecd1ba2cd3f3a63
MERCIER Michael <michael.mercier@inria.fr>
2018-02-07 18:41:13 +0100

    [nix] fix nix dev local

--- default.nix
+++ default.nix
@@ -1,10 +1,10 @@
-{
-  datamove ? import (
-    fetchTarball "https://gitlab.inria.fr/vreis/datamove-nix/repository/master/archive.tar.gz") { }
-}:
-{
-  batsim_local_dev = datamove.batsim_dev.overrideAttrs (attrs: rec {
-    version = "dev_local";
-    src = ./.;
-  });
-}
+with import (
+    fetchTarball
+    "https://gitlab.inria.fr/vreis/datamove-nix/repository/master/archive.tar.gz")
+  { };
+
+batsim_dev.overrideAttrs (attrs: rec {
+  version = "dev_local";
+  src = ./.;
+  buildInputs = attrs.nativeBuildInputs ++ [evalysEnv];
+})
