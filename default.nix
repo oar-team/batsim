64b62d4fd7f36f99c32fbd8d6d0fa31c61831594
MERCIER Michael <michael.mercier@inria.fr>
2018-03-29 18:07:00 +0200

    [code] Now compile with simgrid master head

--- default.nix
+++ default.nix
@@ -2,8 +2,9 @@ with import (
     fetchTarball
     "https://gitlab.inria.fr/vreis/datamove-nix/repository/master/archive.tar.gz")
   { };
+with import <nixpkgs> {};
 
-batsim_dev.overrideAttrs (attrs: rec {
+(batsim_dev.override { batsim = (batsim.override { simgrid = simgrid_dev; }); }).overrideAttrs (attrs: rec {
   version = "dev_local";
   src = ./.;
   buildInputs = attrs.nativeBuildInputs;
