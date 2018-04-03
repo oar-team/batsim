4f167bf8733c998a2cfa114f6b1c0ca4a78922fc
MERCIER Michael <michael.mercier@inria.fr>
2018-04-03 15:34:02 +0200

    [nix] back to simgrid dev version + comment for local use

--- default.nix
+++ default.nix
@@ -1,11 +1,30 @@
 with import (
     fetchTarball
     "https://gitlab.inria.fr/vreis/datamove-nix/repository/master/archive.tar.gz")
-  { };
-with import <nixpkgs> {};
+    { pkgs = (import <nixpkgs> {}); };
+#with import <nixpkgs> {};
 
-(batsim_dev.override { batsim = (batsim.override { simgrid = simgrid_dev; }); }).overrideAttrs (attrs: rec {
+#let
+#  simgrid_local = (pkgs.simgrid_dev.override {
+#  debug = true;
+#  # fortranSupport = true;
+#  # buildDocumentation = true;
+#  # buildJavaBindings = true;
+#  # modelCheckingSupport = true;
+#  #  moreTests = true;
+#  }).overrideAttrs (attrs: {
+#  src = /home/mmercier/Projects/simgrid;
+#  dontStrip = true;
+#  doCheck = false;
+#});
+#in
+(batsim_dev.override {
+  #batsim = (batsim.override { simgrid = simgrid_local; });
+  batsim = (batsim.override { simgrid = simgrid_dev; });
+}).overrideAttrs (attrs: rec {
   version = "dev_local";
   src = ./.;
   buildInputs = attrs.nativeBuildInputs;
+  makeFlags = ["VERBOSE=1"];
+  doCheck = false;
 })
