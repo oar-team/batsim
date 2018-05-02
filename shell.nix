3566838629b0cb6aa81af2f04b7186baacc87f4e
MERCIER Michael <michael.mercier@inria.fr>
2018-05-02 16:32:59 +0200

    [misc] add a simple shell.nix with examples

--- shell.nix
+++ shell.nix
@@ -2,49 +2,22 @@ with import (
     fetchTarball
     "https://gitlab.inria.fr/vreis/datamove-nix/repository/master/archive.tar.gz")
     { };
-# pkgs = (import <nixpkgs> {});
-#with import <nixpkgs> {};
 
-pkgs.stdenv.mkDerivation rec {
-  name = "batsim-test-env";
-  env = pkgs.buildEnv { name = name; paths = buildInputs; };
+batsim_dev
 
-  simgrid_batsim = dmpkgs.simgrid_dev.overrideDerivation(attrs: rec {
-    src = pkgs.fetchgit {
-      url = "https://github.com/simgrid/simgrid.git";
-      rev = "54f07f038966ffe0b3e8ef526e9192f7eb49ad09";
-      sha256 = "0afsh7ys4lcbdabaqzc2m6ddar875f3rrmzjdb00k9x2pr91whpa";
-    };
-    doCheck = false;
-  });
-
-  buildInputs = [
-    #########
-    # Build #
-    #########
-    pkgs.boost
-    pkgs.gmp
-    pkgs.rapidjson
-    pkgs.openssl
-    pkgs.hiredis
-    pkgs.libev
-    pkgs.cppzmq
-    pkgs.zeromq
-    pkgs.cmake
-    simgrid_batsim
-    dmpkgs.redox
-
-    ########
-    # Test #
-    ########
-    # Run simulations
-    dmpkgs.batexpe
-    dmpkgs.batsched_dev
-    pkgs.redis
-    # Check results
-    pkgs.python36
-    pkgs.python36Packages.pandas
-    # Misc
-    pkgs.doxygen
-  ];
-}
+#let
+#  simgrid_local = simgrid_dev.overrideAttrs (attrs: {
+#       src = /path/to/simgrid;
+#       dontStrip = true;
+#       doCheck = false;
+#     }
+#   );
+#in
+#(
+#  batsim_dev.override { simgrid = simgrid_local; }
+#).overrideAttrs (attrs: rec {
+#  version = "dev_local";
+#  src = ./.;
+#  makeFlags = ["VERBOSE=1"];
+#  doCheck = false;
+#})
