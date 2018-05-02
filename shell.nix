719961beb74a48e52a8b0ff4de171f2c596bd8ea
MERCIER Michael <michael.mercier@inria.fr>
2018-05-02 16:32:59 +0200

    [misc] add a simple shell.nix with examples

--- shell.nix
+++ shell.nix
@@ -2,30 +2,22 @@ with import (
     fetchTarball
     "https://gitlab.inria.fr/vreis/datamove-nix/repository/master/archive.tar.gz")
     { };
-# pkgs = (import <nixpkgs> {});
-#with import <nixpkgs> {};
 
+batsim_dev
+
+## For local simgrid development use the folowing instead
+#
 #let
-#  simgrid_local = (pkgs.simgrid_dev.override {
-#  debug = true;
-#  # fortranSupport = true;
-#  # buildDocumentation = true;
-#  # buildJavaBindings = true;
-#  # modelCheckingSupport = true;
-#  #  moreTests = true;
-#  }).overrideAttrs (attrs: {
-#  src = /home/mmercier/Projects/simgrid;
-#  dontStrip = true;
-#  doCheck = false;
-#});
+#  simgrid_local = simgrid_dev.overrideAttrs (attrs: {
+#    src = /path/to/simgrid;
+#    dontStrip = true;
+#    doCheck = false;
+#  });
 #in
-(batsim_dev.override {
-  #batsim = (batsim.override { simgrid = simgrid_local; });
-  batsim = (batsim.override { simgrid = simgrid_dev; });
-}).overrideAttrs (attrs: rec {
-  version = "dev_local";
-  src = ./.;
-  buildInputs = attrs.nativeBuildInputs;
-  makeFlags = ["VERBOSE=1"];
-  doCheck = false;
-})
+#(
+#  batsim_dev.override { simgrid = simgrid_local; }
+#).overrideAttrs (attrs: rec {
+#  version = "dev_local";
+#  src = ./.;
+#  makeFlags = ["VERBOSE=1"];
+#})
