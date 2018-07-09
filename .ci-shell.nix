b22289e7b964f7a1a70898735795286df97a93f0
Millian Poquet <millian.poquet@inria.fr>
2018-07-09 23:06:20 +0200

    [ci] dedicated Nix shell
    As shell.nix is regularly modified, this new shell is dedicated to
    the continuous integration runner and should not be modified often.

---
+++ .ci-shell.nix
@@ -0,0 +1,16 @@
+let
+  pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/18.03.tar.gz") {};
+  dmpkgs = import
+    ( fetchTarball "https://gitlab.inria.fr/vreis/datamove-nix/repository/master/archive.tar.gz")
+  { inherit pkgs; };
+in
+
+with dmpkgs;
+with pkgs;
+
+(batsim_dev.override {}).overrideAttrs (attrs: rec {
+    name = "batsim-ci";
+    src = ./.;
+    enableParallelBuilding = true;
+    doCheck = false;
+})
