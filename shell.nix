dcbca075bccda8de39702fe0ad53fd5d609b9415
MERCIER Michael <michael.mercier@inria.fr>
2018-07-10 18:41:20 +0200

    [nix] more explaination on the shell.nix + add freeze on tracing

--- shell.nix
+++ shell.nix
@@ -1,5 +1,6 @@
 let
-  pkgs = import (     fetchTarball "https://github.com/NixOS/nixpkgs/archive/17.09.tar.gz") {};
+  pkgs = import
+    ( fetchTarball "https://github.com/NixOS/nixpkgs/archive/17.09.tar.gz") {};
   dmpkgs = import
     #~/Projects/datamove-nix
     ( fetchTarball "https://gitlab.inria.fr/vreis/datamove-nix/repository/master/archive.tar.gz")
@@ -15,13 +16,28 @@ with dmpkgs;
 with pkgs;
 let
   simgrid_local = simgrid_dev.overrideAttrs (attrs: {
-    src = ~/Projects/simgrid;
+
+    # Use local project
+    #
+    #src = ~/Projects/simgrid;
+
+    # Use a fork
+    #
     #src = fetchFromGitHub {
     #  owner = "mpoquet";
     #  repo = "simgrid";
     #  rev = "d6d2d67";
     #  sha256 = "00m4a57jf8mrcjn5nld4bdwbnak7rff37bm7ph9c1xmywg02yccy";
     #};
+
+    # Use specific version of simgrid
+    #
+    src = fetchFromGitHub {
+      owner = "simgrid";
+      repo = "simgrid";
+      rev = "b1801e70";
+      sha256 = "0lxmhyapbx252z194pxmydx67cc8hz3p1s5pw28d9x0yip0f9sv2";
+    };
   });
 in
 (
@@ -30,5 +46,5 @@ in
 #batsim_dev.overrideAttrs (attrs: rec {
   version = "dev_local";
   src = ./.;
-  makeFlags = ["VERBOSE=1" "-g"];
+  makeFlags = ["VERBOSE=1"];
 })
