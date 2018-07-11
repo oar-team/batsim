6cdcd274c31fb11dfed03f2409f059b11c74a4f1
Millian Poquet <millian.poquet@inria.fr>
2018-07-11 15:59:48 +0200

    [ci] cleanup

--- ci/update-dependencies-cache.bash
+++ ci/update-dependencies-cache.bash
@@ -4,6 +4,3 @@ set -eu
 
 # (re)build up-to-date CI batsim package, push it on binary cache
 nix-build ci/default.nix | cachix push batsim
-
-# Build up-to-date batsim_dev package, push it on binary cache
-nix-build ${DATAMOVEPKGS:-~/datamovepkgs} -A batsim_dev | cachix push batsim
