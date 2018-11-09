06df6f34f206c0c0e3de0b41e31efcea71a3bcc4
MERCIER Michael <michael.mercier@inria.fr>
2018-11-09 16:51:01 +0100

    [ci] fix nix clang ci script

--- ci/nix-build-clang.bash
+++ ci/nix-build-clang.bash
@@ -3,4 +3,4 @@ set -eu
 
 CI_DIR=$(realpath $(dirname $(realpath $0)))
 # (re)build up-to-date batsim and test dependancies with Clang
-nix-build $CI_DIR --arg kapack "import ~/Projects/kapack {useClang = true;}"
+nix-build $CI_DIR --arg kapack "import (fetchTarball https://github.com/oar-team/kapack/archive/master.tar.gz) {useClang = true;}"
