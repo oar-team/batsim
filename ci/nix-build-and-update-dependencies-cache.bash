7a079c788edcccb971a8b50b84eef05965f5ab9c
MERCIER Michael <michael.mercier@inria.fr>
2018-11-09 13:41:36 +0100

    [ci] refactor nix build scripts
    Do not use the shell (not necessary because everything is imported by
    Nix.
    Only requirement are nix 2.0 and cachix that are set on the docker
    image.

--- ci/nix-build-and-update-dependencies-cache.bash
+++ ci/nix-build-and-update-dependencies-cache.bash
@@ -1,6 +1,5 @@
-#!/usr/bin/env nix-shell
-#! nix-shell -i bash ./default.nix
+#!/usr/bin/env bash
 set -eu
 
 # (re)build up-to-date CI batsim package, push it on binary cache
-nix-build ci/default.nix | cachix push batsim
+nix build -f ci/default.nix | cachix push batsim
