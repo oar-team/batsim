d703b9a0dfbd693b3884cad9a3de17c059e396ec
Millian Poquet <millian.poquet@inria.fr>
2018-11-19 18:38:12 +0100

    [doc] sphinx: require mathjax (may break Nix shell)
    This is to fix a current warning that says that math commands are used
    but that no extension was defined to take charge of them.

--- docs/conf.py
+++ docs/conf.py
@@ -39,7 +39,8 @@ release = ''
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.todo'
+    'sphinx.ext.todo',
+    'sphinx.ext.mathjax'
 ]
 
 todo_include_todos = True
