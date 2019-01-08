72cd1c8c5c9d8aeaa745ec4fa6caeff68c862e34
Millian Poquet <millian.poquet@inria.fr>
2019-01-08 19:33:04 +0100

    [ci] also add batsim build path for unittests

--- ci/run-unittests.bash
+++ ci/run-unittests.bash
@@ -2,6 +2,11 @@
 #! nix-shell -i bash ./default.nix
 set -eux
 
+BUILD_DIR=$(realpath $(dirname $(realpath $0))/../build)
+
+# Add built batsim in PATH
+export PATH=${BUILD_DIR}:${PATH}
+
 # Print which batsim is executed
 echo "batsim realpath: $(realpath $(which batsim))"
 
