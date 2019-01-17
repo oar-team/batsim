5bc253082e52730aa82511c704797feeec4bacb2
Millian Poquet <millian.poquet@inria.fr>
2019-01-17 20:11:52 +0100

    [doc] batlogo on readthedocs ^O^

--- docs/conf.py
+++ docs/conf.py
@@ -75,6 +75,8 @@ pygments_style = 'sphinx'
 
 # -- Options for HTML output -------------------------------------------------
 
+html_logo = './img/logo/logo.png'
+
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
