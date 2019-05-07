1a43559553222c84267ecf596ba1f56377ab2961
Millian Poquet <millian.poquet@inria.fr>
2019-05-07 15:23:27 +0200

    [ci] tuto: use kapack's pkgs + cachix update

--- docs/tuto-first-simulation/tuto-env.nix
+++ docs/tuto-first-simulation/tuto-env.nix
@@ -1,13 +1,11 @@
 let
-  pkgs = import
-    (fetchTarball "https://github.com/NixOS/nixpkgs/archive/18.03.tar.gz") {};
   kapack = import
     ( fetchTarball "https://github.com/oar-team/kapack/archive/master.tar.gz") {};
 in
 
-pkgs.stdenv.mkDerivation rec {
+kapack.pkgs.stdenv.mkDerivation rec {
   name = "tuto-env";
-  env = pkgs.buildEnv { name = name; paths = buildInputs; };
+  env = kapack.pkgs.buildEnv { name = name; paths = buildInputs; };
   buildInputs = [
     kapack.batsim # simulator
     kapack.batsched # scheduler
