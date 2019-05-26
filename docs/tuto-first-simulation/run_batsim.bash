16a10b95d53111b61fc660c9cf366bf70e34ad1c
Millian Poquet <millian.poquet@inria.fr>
2019-05-27 00:16:14 +0200

    [doc] update tuto for batsim-3.1.0

--- docs/tuto-first-simulation/run_batsim.bash
+++ docs/tuto-first-simulation/run_batsim.bash
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-batsim -p /tmp/batsim-v3.0.0/platforms/cluster512.xml \
-       -w /tmp/batsim-v3.0.0/workloads/test_batsim_paper_workload_seed1.json \
+batsim -p /tmp/batsim-v3.1.0/platforms/cluster512.xml \
+       -w /tmp/batsim-v3.1.0/workloads/test_batsim_paper_workload_seed1.json \
        -e "/tmp/expe-out/out"
