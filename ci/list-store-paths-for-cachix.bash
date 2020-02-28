9571a23cd0c4e6d8b1b3b6fe62878d247903b26b
Millian Poquet <millian.poquet@inria.fr>
2020-02-28 22:52:31 +0100

    [misc] clean ci dir + fix list-store-paths script

--- ci/list-store-paths-for-cachix.bash
+++ ci/list-store-paths-for-cachix.bash
@@ -1,10 +1,2 @@
 #!/usr/bin/env bash
-function list_store_paths_for_cachix {
-    var_path=$1
-    echo $var_path | tr ':' '\n' | sed -E -n 'sW(/nix/store/.*)/.*W\1Wp'
-}
-
-list_store_paths_for_cachix ${CMAKE_INCLUDE_PATH}
-list_store_paths_for_cachix ${CMAKE_LIBRARY_PATH}
-list_store_paths_for_cachix ${PATH}
-list_store_paths_for_cachix ${PYTHONPATH}
+env | tr '= :' '\n' | sed -E -n 'sW.*(/nix/store/[a-z0-9]{32}-)W\1Wp' | cut -d '/' -f1-4 | sort -u
--- ci/update-batsim_dev-cache.bash
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env nix-shell
-#! nix-shell -i bash ./default.nix
-set -eu
-
-# Build up-to-date batsim_dev package, push it on binary cache
-nix-build ${KAPACK:-https://github.com/oar-team/kapack/archive/master.tar.gz} -A batsim_dev | cachix push batsim
