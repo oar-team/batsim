13960e34434cdced47b9940607e8f226b6e8d84c
Millian Poquet <millian.poquet@inria.fr>
2020-08-06 12:22:15 +0200

    [tuto] first-simu: manage version properly

--- docs/tuto-first-simulation/run_batsim.bash
+++ docs/tuto-first-simulation/run_batsim.bash
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-batsim -p /tmp/batsim-v3.1.0/platforms/cluster512.xml \
-       -w /tmp/batsim-v3.1.0/workloads/test_batsim_paper_workload_seed1.json \
+batsim -p /tmp/batsim-src-stable/platforms/cluster512.xml \
+       -w /tmp/batsim-src-stable/workloads/test_batsim_paper_workload_seed1.json \
        -e "/tmp/expe-out/out"
