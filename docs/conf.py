6bc21bcb814570916545596d72a7455dac1ea397
Millian Poquet <millian.poquet@inria.fr>
2021-03-16 15:03:11 +0100

    docs: tune conf.py (logo, github integration)

--- docs/conf.py
+++ docs/conf.py
@@ -86,7 +86,18 @@ html_theme = 'sphinx_rtd_theme'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    'logo_only': True,
+    'display_version': False,
+}
+
+html_context = {
+    "display_github": True,
+    "github_user": "oar-team",
+    "github_repo": "batsim",
+    "github_version": "master",
+    "conf_py_path": "/docs/",
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
