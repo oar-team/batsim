d81a8815b382c61c9be10699fd423a7348aec83e
Millian Poquet <millian.poquet@inria.fr>
2021-06-04 00:23:32 +0200

    nix: use pytest-html from nixpkgs

--- default.nix
+++ default.nix
@@ -112,7 +112,7 @@ let
       ];
       buildInputs = with pkgs.python3Packages; [
         batsim batsched batexpe pkgs.redis
-        pybatsim pytest pytest_html pandas] ++
+        pybatsim pytest pytest-html pandas] ++
       pkgs.lib.optional doValgrindAnalysis [ pkgs.valgrind ];
 
       pytestArgs = "-ra test/ --html=./report/pytest_report.html" +
@@ -279,33 +279,6 @@ let
         mkdir -p $out
         cp -r _build/html $out/
       '';
-    };
-
-    # Dependencies not in nixpkgs as I write these lines.
-    pytest_metadata = buildPythonPackage {
-      name = "pytest-metadata-1.8.0";
-      doCheck = false;
-      propagatedBuildInputs = [
-        pythonPackages.pytest
-        pythonPackages.setuptools_scm
-      ];
-      src = builtins.fetchurl {
-        url = "https://files.pythonhosted.org/packages/12/38/eed3a1e00c765e4da61e4e833de41c3458cef5d18e819d09f0f160682993/pytest-metadata-1.8.0.tar.gz";
-        sha256 = "1fk6icip2x1nh4kzhbc8cnqrs77avpqvj7ny3xadfh6yhn9aaw90";
-      };
-    };
-
-    pytest_html = buildPythonPackage {
-      name = "pytest-html-1.20.0";
-      doCheck = false;
-      propagatedBuildInputs = [
-        pythonPackages.pytest
-        pytest_metadata
-      ];
-      src = builtins.fetchurl {
-        url = "https://files.pythonhosted.org/packages/08/3e/63d998f26c7846d3dac6da152d1b93db3670538c5e2fe18b88690c1f52a7/pytest-html-1.20.0.tar.gz";
-        sha256 = "17jyn4czkihrs225nkpj0h113hc03y0cl07myb70jkaykpfmrim7";
-      };
     };
   };
 in
