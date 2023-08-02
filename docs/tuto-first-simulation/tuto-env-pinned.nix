a30f93c840f13d3ffd17afcc67b67dc9e8385821
Millian Poquet <millian.poquet@irit.fr>
2023-08-02 16:45:10 +0200

    tuto: update pinned tuto env w.r.t. batsim-4.2.0

--- docs/tuto-first-simulation/tuto-env-pinned.nix
+++ docs/tuto-first-simulation/tuto-env-pinned.nix
@@ -4,17 +4,17 @@ let
 in
 
 let my-packages = rec {
-  # define your own batsim version from batsim-4.1.0
+  # define your own batsim version from batsim-4.2.0
   # (you can select another base, such as batsim-310 for batsim-3.1.0)
-  my-batsim = kapack.batsim-410.overrideAttrs(old: rec {
+  my-batsim = kapack.batsim-420.overrideAttrs(old: rec {
     # define where to the source code of your own batsim version.
     # here, use a given commit on the official batsim git repository on framagit,
     # but you can of course use your own commit/fork instead.
-    version = "3e7a7e5ecf1b749c306c532f828219e5a3d70862";
+    version = "194c926d4c9c9c83340ef9d5ba17ad47e8b3b67e";
     src = kapack.pkgs.fetchgit rec {
       url = "https://framagit.org/batsim/batsim.git";
       rev = version;
-      sha256 = "1f3xij13i4wgd667n0sqdkkzn5b11fz4bmxv9s0yivdniwn6md76";
+      sha256 = "sha256-RsjvPhuJL6S804/BZGpzl9qkCujWJxWK0isD4osekMI=";
     };
   });
 
@@ -25,7 +25,7 @@ let my-packages = rec {
     src = kapack.pkgs.fetchgit rec {
       url = "https://framagit.org/batsim/batsched.git";
       rev = version;
-      sha256 = "1dmx2zk25y24z3m92bsfndvvgmdg4wy2iildjmwr3drmw15s75q0";
+      sha256 = "sha256-AJejS+A1t5F5lY3GKDwnr9W3d7NOL5Hq+ET4IuYXvbY=";
     };
   });
 
@@ -37,7 +37,7 @@ let my-packages = rec {
     src = kapack.pkgs.fetchgit rec {
       url = "https://gitlab.inria.fr/batsim/pybatsim.git";
       rev = version;
-      sha256 = "0nr8hqsx6y1k0lv82f4x3fjq6pz2fsd3h44cvnm7w21g4v3s6l6y";
+      sha256 = "sha256-3lCjxyYvCH6q3YwQOJp24l+DpRudOIE2BTN40zWGKFs=";
     };
   });
 
